a8094dde7b9f94843d6faadc18130b486ebc5dd0
mohamed7omran <mo1234512345ed@gmail.com>
2026-04-26 15:22:31 +0300

    create User table

---
+++ apps/api/src/user/dto/create-user.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  userID: string;
+
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  role: string;
+}
